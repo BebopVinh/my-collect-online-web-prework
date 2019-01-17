56ccfed7f2544277d1c93b387dc5c758fe16fa1b
Vinh Do <vinhdo07@gmail.com>
2019-01-17 23:46:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(array)
   i = 0
+  new_array = []
   while i < array.size
     yield(array[i])
+    new_array << array[i]
     i += 1
   end
 end
 
-my_collect()
+my_collect(1) {|i| i.capitalized}
